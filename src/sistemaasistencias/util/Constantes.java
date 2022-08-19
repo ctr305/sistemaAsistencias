a96ab7a1a6a25e8d55d30b9fea8b3f92e85bcc0f
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-18 20:38:02 -0500

    Añadir archivos de utilidades.

---
+++ src/sistemaasistencias/util/Constantes.java
@@ -0,0 +1,15 @@
+/*
+* Autor: Leonardo Criollo Ramírez
+* Fecha de creacion: 29/05/22
+* Fecha de modificacion: 15/06/22
+* Descripcion: Clase contenedora de constantes para el proyecto.
+*/
+package proyectoconstruccion.util;
+
+public class Constantes {
+    
+    public static final int CODIGO_ERROR_CONEXIONBD = 500;
+    public static final int CODIGO_OPERACION_CORRECTA = 200;
+    public static final int CODIGO_CREDENCIALES_INCORRECTAS = 401;
+    public static final int CODIGO_OPERACION_DML_FALLIDA = 402;
+}
