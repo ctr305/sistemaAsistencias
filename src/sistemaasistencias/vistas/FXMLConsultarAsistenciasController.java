b047eb4f85dc6104bc8f8c95d63d003a793e0b5a
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 18:17:43 -0500

    Añadir ventana de consulta de asistencias.

---
+++ src/sistemaasistencias/vistas/FXMLConsultarAsistenciasController.java
@@ -0,0 +1,18 @@
+/*
+ * Autor: Leonardo Criollo Ramirez
+ * Fecha de creacion: 22/08/22
+ * Fecha de modificacion: 22/08/22
+ * Descripcion: Controlador de la ventana de consulta de asistencias
+ */
+package sistemaasistencias.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLConsultarAsistenciasController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+}
