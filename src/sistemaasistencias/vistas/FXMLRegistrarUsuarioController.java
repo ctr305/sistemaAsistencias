d87239d1bdccb7b1e13300b0b48b811f8df1b3f1
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 09:04:10 -0500

    Crear ventana de registro de usuario.

---
+++ src/sistemaasistencias/vistas/FXMLRegistrarUsuarioController.java
@@ -0,0 +1,21 @@
+/*
+ * Autor: Leonardo Criollo Ramirez
+ * Fecha de creacion: 22/08/22
+ * Fecha de modificacion: 22/08/22
+ * Descripcion: Controlador de la ventana de registro de usuario.
+ */
+package sistemaasistencias.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLRegistrarUsuarioController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
