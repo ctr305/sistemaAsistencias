e6b03f097b4ac691bfd00fc3e9fa128ae31349bd
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-21 19:46:12 -0500

    Añadir test para DAO de Asistencia.

---
+++ test/sistemaasistencias/modelo/DAO/AsistenciaDAOTest.java
@@ -0,0 +1,56 @@
+/*
+ * Autor: Leonardo Criollo Ramirez
+ * Fecha de creacion: 21/08/22
+ * Fecha de modificacion: 21/08/22
+ * Descripcion: Prueba del DAO de Asistencia
+ */
+package sistemaasistencias.modelo.DAO;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import sistemaasistencias.modelo.POJO.Asistencia;
+import sistemaasistencias.modelo.POJO.Clase;
+
+public class AsistenciaDAOTest {
+
+    public AsistenciaDAOTest(){
+    }
+    
+    @Test
+    public void testGetAsistencias() {
+        Clase clase = new Clase();
+        clase.setIdClase(1);
+        
+        ArrayList<Asistencia> expResult = new ArrayList<>();
+        Asistencia asistenciaExp = new Asistencia();
+        asistenciaExp.setIdAsistencia(1);
+        asistenciaExp.setIdClase(1);
+        asistenciaExp.setIdUsuario("S00000000");
+        expResult.add(asistenciaExp);
+        
+        ArrayList<Asistencia> result = AsistenciaDAO.getAsistencias(clase);
+        assertEquals(expResult.get(0).getIdAsistencia(), result.get(0).getIdAsistencia());
+        assertEquals(expResult.get(0).getIdClase(), result.get(0).getIdClase());
+        assertEquals(expResult.get(0).getIdUsuario(), result.get(0).getIdUsuario());
+    }
+    
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testGetAsistencias() {
+        Clase clase = new Clase();
+        clase.setIdClase(2);
+        
+        ArrayList<Asistencia> expResult = new ArrayList<>();
+        Asistencia asistenciaExp = new Asistencia();
+        asistenciaExp.setIdAsistencia(0);
+        asistenciaExp.setIdClase(2);
+        asistenciaExp.setIdUsuario("S00000001");
+        expResult.add(asistenciaExp);
+        
+        ArrayList<Asistencia> result = AsistenciaDAO.getAsistencias(clase);
+        assertEquals(expResult.get(0).getIdAsistencia(), result.get(0).getIdAsistencia());
+        assertEquals(expResult.get(0).getIdClase(), result.get(0).getIdClase());
+        assertEquals(expResult.get(0).getIdUsuario(), result.get(0).getIdUsuario());
+    }
+ }
