1e2bd0a1dd751a58e1c48d885442d1dd55313d9b
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 18:55:56 -0500

    Crear ventana de lista de asistencias.

---
+++ src/sistemaasistencias/vistas/FXMLListaAsistenciasController.java
@@ -0,0 +1,19 @@
+/*
+ * Autor: Leonardo Criollo Ramirez
+ * Fecha de creacion: 22/08/22
+ * Fecha de modificacion: 22/08/22
+ * Descripcion: Controlador de la ventana de lista de assitencias
+ */
+package sistemaasistencias.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLListaAsistenciasController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
