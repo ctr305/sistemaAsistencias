2da89b97a5c1d0145e24af1906454dbaa471c166
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 18:50:37 -0500

    Implementar GUI de seleccion de Experiencia Educativa.

--- src/sistemaasistencias/vistas/FXMLConsultarAsistenciasController.java
+++ src/sistemaasistencias/vistas/FXMLConsultarAsistenciasController.java
@@ -7,12 +7,68 @@
 package sistemaasistencias.vistas;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import sistemaasistencias.modelo.DAO.ExperienciaEducativaDAO;
+import sistemaasistencias.modelo.POJO.ExperienciaEducativa;
+import sistemaasistencias.util.Utilidades;
 
 public class FXMLConsultarAsistenciasController implements Initializable {
 
+    @FXML
+    private TableView<ExperienciaEducativa> tbExperienciasEducativas;
+    @FXML
+    private TableColumn colNRC;
+    @FXML
+    private TableColumn colNombre;
+    
+    private ObservableList<ExperienciaEducativa> infoExperienciasEducativas;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        configurarColumnas();
+        cargarInformacion();
     }    
+
+    @FXML
+    private void btnCancelar(ActionEvent event) {
+        cerrarVentana();
+    }
+    
+    private void configurarColumnas(){
+        colNRC.setCellFactory(new PropertyValueFactory("NRC"));
+        colNombre.setCellFactory(new PropertyValueFactory("nombre"));
+        infoExperienciasEducativas = FXCollections.observableArrayList();
+    }
+    
+    private void cargarInformacion(){
+        ArrayList<ExperienciaEducativa> resultadoConsulta = ExperienciaEducativaDAO.getExperienciasEducativas();
+        if(resultadoConsulta != null){
+            infoExperienciasEducativas.clear();
+            infoExperienciasEducativas.addAll(resultadoConsulta);
+            tbExperienciasEducativas.setItems(infoExperienciasEducativas);
+        }else{
+            Utilidades.mostrarAlerta("Error de conexion", 
+                        "No existe conexion con la base de datos.",
+                        Alert.AlertType.ERROR);
+            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, new RuntimeException());
+        }
+    }
+    
+    private void cerrarVentana(){
+        Stage escenario = (Stage) tbExperienciasEducativas.getScene().getWindow();
+        escenario.close();
+    }
 }
