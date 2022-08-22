8b1e8e13a650f6cc0c9bf3f5bc1eeedd673a539f
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 17:48:11 -0500

    Implementar funcionalidad de menú.

--- src/sistemaasistencias/vistas/FXMLMenuController.java
+++ src/sistemaasistencias/vistas/FXMLMenuController.java
@@ -6,13 +6,25 @@
  */
 package sistemaasistencias.vistas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import sistemaasistencias.util.Utilidades;
 
 public class FXMLMenuController implements Initializable {
+
+    @FXML
+    private Label lbMenu;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -20,10 +32,39 @@ public class FXMLMenuController implements Initializable {
 
     @FXML
     private void btnRegistrarUsuario(ActionEvent event) {
+        abrirRegistroUsuarios();
     }
 
     @FXML
     private void btnVerAsistencia(ActionEvent event) {
+        abrirConsultaAsistencias();
     }
     
+    private void abrirRegistroUsuarios() {
+        try{
+            Stage escenarioPrincipal = (Stage) lbMenu.getScene().getWindow();
+            Scene menu = new Scene(FXMLLoader.load(getClass().getResource("FXMLRegistrarUsuario.fxml")));
+            escenarioPrincipal.setScene(menu);
+            escenarioPrincipal.show();
+        } catch(IOException e){
+            Utilidades.mostrarAlerta("Error",
+                    "Ocurrio un error al mostrar la ventana",
+                    Alert.AlertType.ERROR);
+            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    private void abrirConsultaAsistencias() {
+        try{
+            Stage escenarioPrincipal = (Stage) lbMenu.getScene().getWindow();
+            Scene menu = new Scene(FXMLLoader.load(getClass().getResource("FXMLConsultarAsistencias.fxml")));
+            escenarioPrincipal.setScene(menu);
+            escenarioPrincipal.show();
+        } catch(IOException e){
+            Utilidades.mostrarAlerta("Error",
+                    "Ocurrio un error al mostrar la ventana",
+                    Alert.AlertType.ERROR);
+            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 }
