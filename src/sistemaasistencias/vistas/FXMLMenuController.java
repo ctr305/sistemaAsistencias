68aafbf573d3c0dde9195eadca030ddb217fee98
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 08:25:23 -0500

    Añadir FXML y controlador de la ventana de menú.

---
+++ src/sistemaasistencias/vistas/FXMLMenuController.java
@@ -0,0 +1,20 @@
+/*
+ * Autor: Leonardo Criollo Ramirez
+ * Fecha de creacion: 22/08/22
+ * Fecha de modificacion: 22/08/22
+ * Descripcion: Controlador de la ventana de incio de sesión
+ */
+package sistemaasistencias.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
