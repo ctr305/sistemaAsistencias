d03772a6605652e4c70825fe6666af8e5ccb54a6
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-08-22 08:13:30 -0500

    Implementar chequeos para incio de sesión.

--- src/sistemaasistencias/vistas/FXMLInicioSesionController.java
+++ src/sistemaasistencias/vistas/FXMLInicioSesionController.java
@@ -8,11 +8,18 @@ package sistemaasistencias.vistas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import sistemaasistencias.modelo.DAO.UsuarioDAO;
+import sistemaasistencias.modelo.POJO.Usuario;
+import sistemaasistencias.util.Constantes;
+import sistemaasistencias.util.Utilidades;
 
 public class FXMLInicioSesionController implements Initializable {
 
@@ -28,6 +35,44 @@ public class FXMLInicioSesionController implements Initializable {
 
     @FXML
     private void btnInicioSesion(ActionEvent event) {
+        String usuario = tfNombreUsuario.getText();
+        String password = pfPassword.getText();
+        pfPassword.clear();
+        
+        if(ValidarCampos(usuario, password)){
+            AbrirVentanaPrincipal();
+        }
+    }
+
+    private boolean ValidarCampos(String usuario, String password) {
+        Usuario.usuarioLogin = UsuarioDAO.IniciarSesion(usuario, password);
+        boolean resultado = false;
+        
+        switch(Usuario.usuarioLogin.getCodigoRespuesta()){
+            case Constantes.CODIGO_OPERACION_CORRECTA:
+                resultado = true;
+                break;
+            case Constantes.CODIGO_CREDENCIALES_INCORRECTAS:
+                Utilidades.mostrarAlerta("Error de autenticacion",
+                        "Las credenciales introducidas no son correctas.",
+                        Alert.AlertType.WARNING);
+                break;
+            case Constantes.CODIGO_ERROR_CONEXIONBD:
+                Utilidades.mostrarAlerta("Error de conexion", 
+                        "No existe conexion con la base de datos.",
+                        Alert.AlertType.ERROR);
+                break;
+            default:
+                Utilidades.mostrarAlerta("Error", 
+                        "Ocurrio un error desconocido.", 
+                        Alert.AlertType.ERROR);
+                Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, new RuntimeException());
+        }
+        return resultado;
+    }
+
+    private void AbrirVentanaPrincipal() {
+        //TODO
     }
     
 }
